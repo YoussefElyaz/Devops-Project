33c48609af0184664d1567ba5b7614dca61c2791
Youssef ELYAZIGHI <56431002+azphalte@users.noreply.github.com>
2021-12-21 23:41:59 +0100

    Comment Index.js
    For a better comprehension of this file

--- userapi/src/index.js
+++ userapi/src/index.js
@@ -1,3 +1,4 @@
+//Déclarer les Consts(les modules, port, serveur et les routes)
 const express = require('express')
 const userRouter = require('./routes/user')
 const bodyParser = require('body-parser')
@@ -10,15 +11,19 @@ db.on("error", (err) => {
   console.error(err)
 })
 
+//Utiliser les modules déclarés 
 app.use(bodyParser.urlencoded({
   extended: false
 }))
 app.use(bodyParser.json())
 
+// Default Root: [ http://localhost:5000/ ] 
 app.get('/', (req, res) => res.send('Welcome to Youssef and Augustin Project'))
 
+//Utiliser les routes crées
 app.use('/user', userRouter)
 
+// le serveur écoute sur le PORT
 const server = app.listen(port, (err) => {
   if (err) throw err
   console.log("Server listening the port " + port)
